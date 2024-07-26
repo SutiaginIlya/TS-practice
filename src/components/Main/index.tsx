455172725cdfc0ed8958fa60e400509b09960acb
Сутягин Илья <ilya.sutiagin2012@yandex.ru>
2024-07-26 15:51:55 +0400

    fix: add "Main" component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,19 @@
-
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import styles from "./index.module.css"
 
 export const Main = () => {
     return(
-        <div>
+        <div className={styles.main}>
+            <div className={styles.main_text}>
+                <p>List of Users</p>
+            </div>
+            <div className={styles.users_cont}>
+                <Button variant="contained" endIcon={<SendIcon />}>
+                    Get Users
+                </Button>
+                <input type='text' placeholder="Find..." className={styles.input}></input>
+            </div>
         </div>
     )
 }
