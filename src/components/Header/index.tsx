f2ffe05c12846cebf244c507e7f3a9b045d69026
Сутягин Илья <ilya.sutiagin2012@yandex.ru>
2024-07-26 15:02:08 +0400

    fix: create header & css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
-
+import logo from "../../logo.png"
+import styles from "./index.module.css"
 
 export const Header = () => {
-    return
+    return(
+        <div className={styles.header}>
+            <div className={styles.logo_cont}>
+                <a href="">
+                    <img src={logo} alt="logo" className={styles.logo}/>
+                </a>
+            </div>
+            <span className={styles.text_cont}>Type Script</span>
+        </div>
+    )
 }
